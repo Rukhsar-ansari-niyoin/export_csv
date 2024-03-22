614352a20185fe444b0cc75566a69e928f750356
Rukhsar-ansari-niyoin <157568936+Rukhsar-ansari-niyoin@users.noreply.github.com>
2024-03-22 11:53:01 +0530

    fgfg

--- database/migrations/2024_03_12_081030_create_data_table.php
+++ database/migrations/2024_03_12_081030_create_data_table.php
@@ -13,20 +13,22 @@ return new class extends Migration
     {
         Schema::create('data', function (Blueprint $table) {
             $table->id();
-            $table->string('merchant_code');
-            $table->string('name');
-            $table->string('phone');
-            $table->string('email');
-            $table->string('firm_name');
-            $table->string('pincode');
+            $table->string('merchant_code')->nullable();
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('firm_name')->nullable();
+            $table->string('pincode')->nullable();
             $table->boolean('is_activated')->default(0);
+            $table->boolean('mobile_is_activated')->default(0);
             $table->integer('email_otp')->nullable();
-            $table->string('state');
-            $table->date('dob');
-            $table->string('address');
-            $table->string('type');
-            $table->string('pancard');
-            $table->string('pancardImg');
+            $table->integer('mobile_otp')->nullable();      
+            $table->string('state')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('address')->nullable();
+            $table->string('type')->nullable();
+            $table->string('pancard')->nullable();
+            $table->string('pancardImg')->nullable();
             $table->string('aadhar')->nullable();
             $table->string('aadharFimg')->nullable();
             $table->string('aadharBimg')->nullable();
